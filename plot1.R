a334d9c24b2a48d5eac9562ab6d309ed7c86aa1e
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-04 16:01:15 -0300

    st2a

---
+++ plot1.R
@@ -0,0 +1,21 @@
+##This assignment uses data from the UC Irvine Machine Learning Repository, a popular repository for machine learning datasets. In particular, we will be using the “Individual household electric power consumption Data Set” which I have made available on the course web site.
+##reading the file
+consumo <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+#Organizing the data
+consumo$Date <- as.Date(consumo$Date, "%d/%m/%Y")
+consumo <- subset(consumo,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+consumo <- consumo[complete.cases(consumo),]
+dttempo <- paste(consumo$Date, consumo$Time)
+
+##Naming it
+dttempo <- setNames(dttempo, "Time")
+consumo <- consumo[ ,!(names(consumo) %in% c("Date","Time"))]
+consumo <- cbind(dttempo, consumo)
+
+consumo$dttempo <- as.POSIXct(dttempo)
+
+##plot1
+hist(consumo$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
